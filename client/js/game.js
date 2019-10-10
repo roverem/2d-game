86e20eeeb49f763699eebc413d54ae669403b5ed
Martin Rovere <martinr@qb9.net>
2019-10-10 16:49:03 -0300

    server client comunican

--- client/js/game.js
+++ client/js/game.js
@@ -9,6 +9,12 @@ export class Game{
         console.log(APP);
 
         let t = new PIXI.Text("some text",{fontFamily: 'Arial', fontSize: 34, fill: 0xffffff, align: 'center'});
+        t.anchor.set(0.5);
+        t.x = APP.renderer.width / 2;
+        t.y = APP.renderer.height / 2;
         APP.stage.addChild(t);
+
+
+        SOCKET.emit( "player_ready", {ready:true} );
     }
 }
--- server/main.js
+++ server/main.js
@@ -9,17 +9,22 @@ const IO = io(HTTP);
 
 APP.use('/', express.static("./client"));
 
-console.log(APP);
-
-
 IO.on('connection', function(socket){
 	console.log("user connected");
 	
 	socket.on('disconnect', function(){
 		console.log("user disconnected");
 	});
+
+
+	socket.on( 'player_ready', player_ready.bind(this) );
 });
 
 HTTP.listen(process.env.PORT || 3000, function(){
 	console.log('listening on *:3000');
 });
+
+
+function player_ready(data){
+	console.log(data);
+}
