afee0421ca186c5fce422da6dfdd5889d5de7c9f
Martin Rovere <martinr@qb9.net>
2019-10-10 14:53:46 -0300

    first commit of sorts

--- client/js/game.js
+++ client/js/game.js
@@ -7,5 +7,8 @@ export class Game{
 
         console.log(SOCKET);
         console.log(APP);
+
+        let t = new PIXI.Text("some text",{fontFamily: 'Arial', fontSize: 34, fill: 0xffffff, align: 'center'});
+        APP.stage.addChild(t);
     }
 }
